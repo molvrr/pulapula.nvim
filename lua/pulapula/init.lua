622ab21862d7e0f62f87d7b7422465b7d59f0430
Mateus Cruz <mateuscolvr@gmail.com>
2023-05-29 19:45:01 -0300

    refactor

--- lua/pulapula/init.lua
+++ lua/pulapula/init.lua
@@ -1,71 +1,53 @@
 local M = {}
 
 M.navigate = function(dir)
+  local tmux = os.getenv('TMUX')
   local statusline_height = 1
   local cmdline_height = vim.o.cmdheight
   local pos = vim.api.nvim_win_get_position(0)
-  local row = pos[1]
-  local col = pos[2]
   local width = vim.api.nvim_win_get_width(0)
   local height = vim.api.nvim_win_get_height(0)
-
   local active_socket = vim.split(vim.fn.expand('$TMUX'), ',')[1]
-
   local pane = vim.fn.expand('$TMUX_PANE')
+  local tmux_cmd = string.format('tmux -S %s select-pane -t %s -%s', active_socket, pane, dir)
 
-  if dir[1] == ' -L' then
-    if col == 0 then
-      local tmux_cmd = string.format('tmux -S %s select-pane -t %s %s', active_socket, pane, dir[1])
-
+  if dir == 'L' then
+    if pos[2] == 0 and tmux then
       vim.fn.system(tmux_cmd)
     else
-      local keys = vim.api.nvim_replace_termcodes('<C-w>h', true, false, true)
-      vim.fn.feedkeys(keys)
+      vim.cmd.wincmd('h')
     end
-  elseif dir[1] == ' -R' then
-    if col + width == vim.o.columns then
-      local tmux_cmd = string.format('tmux -S %s select-pane -t %s %s', active_socket, pane, dir[1])
-
+  elseif dir == 'R' then
+    if pos[2] + width == vim.o.columns and tmux then
       vim.fn.system(tmux_cmd)
     else
-      local keys = vim.api.nvim_replace_termcodes('<C-w>l', true, false, true)
-      vim.fn.feedkeys(keys)
+      vim.cmd.wincmd('l')
     end
-  elseif dir[1] == ' -D' then
-    if row + height + statusline_height + cmdline_height == vim.o.lines then
-      local tmux_cmd = string.format('tmux -S %s select-pane -t %s %s', active_socket, pane, dir[1])
-
+  elseif dir == 'U' then
+    if pos[1] == 0 and tmux then
       vim.fn.system(tmux_cmd)
     else
-      local keys = vim.api.nvim_replace_termcodes('<C-w>j', true, false, true)
-      vim.fn.feedkeys(keys)
+      vim.cmd.wincmd('k')
     end
-  elseif dir[1] == ' -U' then
-    if row == 0 then
-      local tmux_cmd = string.format('tmux -S %s select-pane -t %s %s', active_socket, pane, dir[1])
-
+  elseif dir == 'D' then
+    if pos[1] + height + statusline_height + cmdline_height == vim.o.lines and tmux then
       vim.fn.system(tmux_cmd)
     else
-      local keys = vim.api.nvim_replace_termcodes('<C-w>k', true, false, true)
-      vim.fn.feedkeys(keys)
+      vim.cmd.wincmd('j')
     end
   end
 end
 
-M.left = function()
-  M.navigate({' -L'})
-end
+M.maximize = function()
+  local tmux = os.getenv('TMUX')
+  local windows = vim.api.nvim_tabpage_list_wins(0)
+  local active_socket = vim.split(vim.fn.expand('$TMUX'), ',')[1]
 
-M.right = function()
-  M.navigate({' -R'})
-end
-
-M.up = function()
-  M.navigate({' -U'})
-end
-
-M.down = function()
-  M.navigate({' -D'})
+  if #windows == 1 and tmux then
+    vim.fn.system(string.format('tmux -S %s resize-pane -Z', active_socket))
+  else
+    vim.cmd.only()
+  end
 end
 
 return M
