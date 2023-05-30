611fe8ca09852c94f63cef929419de019c3d16db
Mateus Cruz <mateuscolvr@gmail.com>
2023-05-29 22:13:52 -0300

    refactor: remove ifs

--- lua/pulapula/init.lua
+++ lua/pulapula/init.lua
@@ -1,44 +1,32 @@
-local M = {}
-
-M.navigate = function(dir)
-  local tmux = os.getenv('TMUX')
-  local statusline_height = 1
-  local cmdline_height = vim.o.cmdheight
+local dirs = function()
   local pos = vim.api.nvim_win_get_position(0)
   local width = vim.api.nvim_win_get_width(0)
   local height = vim.api.nvim_win_get_height(0)
+
+  return {
+    L = { key = 'h', move = pos[2] == 0 },
+    R = { key = 'l', move = pos[2] + width == vim.o.columns },
+    U = { key = 'k', move = pos[1] == 0 },
+    D = { key = 'j', move = pos[1] + height + 1 + vim.o.cmdheight == vim.o.lines }
+  }
+end
+
+local navigate = function(dir)
+  local tmux = os.getenv('TMUX')
   local active_socket = vim.split(vim.fn.expand('$TMUX'), ',')[1]
   local pane = vim.fn.expand('$TMUX_PANE')
   local tmux_cmd = string.format('tmux -S %s select-pane -t %s -%s', active_socket, pane, dir)
+  local zoomed = string.match(vim.fn.system("tmux list-panes -F '#F'"), 'Z')
+  local should_move = tmux and not (zoomed and not vim.g.pulapula_ignore_zoom) and dirs()[dir].move
 
-  if dir == 'L' then
-    if pos[2] == 0 and tmux then
-      vim.fn.system(tmux_cmd)
-    else
-      vim.cmd.wincmd('h')
-    end
-  elseif dir == 'R' then
-    if pos[2] + width == vim.o.columns and tmux then
-      vim.fn.system(tmux_cmd)
-    else
-      vim.cmd.wincmd('l')
-    end
-  elseif dir == 'U' then
-    if pos[1] == 0 and tmux then
-      vim.fn.system(tmux_cmd)
-    else
-      vim.cmd.wincmd('k')
-    end
-  elseif dir == 'D' then
-    if pos[1] + height + statusline_height + cmdline_height == vim.o.lines and tmux then
-      vim.fn.system(tmux_cmd)
-    else
-      vim.cmd.wincmd('j')
-    end
+  if should_move then
+    return vim.fn.system(tmux_cmd)
+  else
+    vim.cmd.wincmd(dirs()[dir].key)
   end
 end
 
-M.maximize = function()
+local maximize = function()
   local tmux = os.getenv('TMUX')
   local windows = vim.api.nvim_tabpage_list_wins(0)
   local active_socket = vim.split(vim.fn.expand('$TMUX'), ',')[1]
@@ -50,4 +38,7 @@ M.maximize = function()
   end
 end
 
-return M
+return {
+  maximize = maximize,
+  navigate = navigate,
+}
